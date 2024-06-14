adb17cab69ad597bd11be2fd5465ce27c30fac31
AntoineSinety <antoine.desinety@gmail.com>
2024-06-14 23:33:39 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tyfawil-v2/sw.js', { scope: '/tyfawil-v2/' })})}
